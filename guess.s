c6e942c0fd56f908a0844a6c5d692951a43fe631
StevenL94 <steven_lee_13@hotmail.com>
2016-02-17 19:45:43 -0500

    Updated Code

--- guess.s
+++ guess.s
@@ -7,28 +7,50 @@ start:
     movb $0x03,%al      # 0x03 - 80x25 text mode
     int $0x10           # call into the BIOS
 
-get_time:
-    outb %al, $0x70
-    inb $0x71, %al
-
 print_char: 
     lodsb           # loads a single byte from (%si) into %al and increments %si
     testb %al,%al   # checks to see if the byte is 0
-    jz done         # if so, jump out (jz jumps if ZF in EFLAGS is set)
+    jz user_input         # if so, jump out (jz jumps if ZF in EFLAGS is set)
     movb $0x0E,%ah  # 0x0E is the BIOS code to print the single character
     int $0x10       # call into the BIOS using a software interrupt
     jmp print_char  # go back to the start of the loop
 
+get_time:
+    outb %al, $0x70
+    inb $0x71, %al
+    and 0x0F, %al
+    movl %al, %bl
+    jmp user_input
+
 user_input:
     movb $0x00,%ah
     int $0x16
+
+test_input:
+    test %al, %bl
+    jne print_char
+    je correct
+
+wrong:
+    movw $message1, %si
+    jmp print_char
+correct:
+    movw $message1, %si
+    jmp print_char
 
 done: 
     jmp done        # loop forever
 
 # The .string command inserts an ASCII string with a null terminator
 message:
-    .string    "What number am I thinking of (0-9)?"
+    .string    "What number am I thinking of (0-9)? "
+
+message1:
+    .string    "Wrong!"
+
+message2:
+    .string    "Right! Congratulations."
+
 
 # This pads out the rest of the boot sector and then puts
 # the magic 0x55AA that the BIOS expects at the end, making sure
