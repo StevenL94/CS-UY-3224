27993b5e824352c97c0abe58c80ae865c84991cf
Steven Lee <sl3881@nyu.edu>
2016-04-28 22:19:50 -0400

    Added preliminary password testing - Does not work

--- Homework 8/xv6-public/init.c
+++ Homework 8/xv6-public/init.c
@@ -15,6 +15,11 @@ main(void)
   int pid, wpid;
   static char buf[100];
   int buffread = 0;
+  char readbuf[512];
+  int n;
+  uchar salt[128];
+  uchar* hash;
+  int h_full;
     
   if(open("console", O_RDWR) < 0){
     mknod("console", 1, 1);
@@ -37,9 +42,7 @@ main(void)
             fd = open("pw", O_CREATE);
             printf(1, "Enter password: ");
             sgenrand(1024);
-            uchar salt[128];
-            *salt = genrand()*genrand()*genrand()*genrand();
-            uchar* hash;
+            *salt = genrand();
             while (buffread >= 0) {
                 memset(buf, 0, sizeof(buf));
                 gets(buf, sizeof(buf));
@@ -78,6 +81,7 @@ main(void)
                     if(buf[0] == 0) // EOF
                         buffread = -1;
                     buf[strlen(buf)-1] = 0;
+                    break;
                 }
                 write(fd, hash, O_WRONLY);
                 printf(1, "Password successfully set. You may now use it to log in.\n");
@@ -85,6 +89,46 @@ main(void)
             write(fd, hash, O_WRONLY);
             printf(1, "Password successfully set. You may now use it to log in.\n");
         }
+        while((n = read(fd, readbuf, sizeof(readbuf))) > 0) {
+            write(1, readbuf, n);
+        }
+        int i;
+        h_full = 0;
+        for(i = 0; i < sizeof(readbuf); ++i){
+            if (h_full == 0) {
+                if (readbuf[i] != '\n') {
+                    hash[i] = readbuf[i];
+                }
+                else {
+                    h_full = 1;
+                }
+            }
+            ++i;
+            if (readbuf[i] != '\n') {
+                salt[i] = readbuf[i];
+            }
+        }
+        printf(1, "Enter password: ");
+        while (buffread >= 0) {
+            memset(buf, 0, sizeof(buf));
+            gets(buf, sizeof(buf));
+            if(buf[0] == 0) // EOF
+                buffread = -1;
+            buf[strlen(buf)-1] = 0;
+            break;
+        }
+        while (bcrypt_checkpass(buf, salt, hash) < 0) {
+            printf(1, "Passwords do not match. Try again.\nEnter password: ");
+            while (buffread >= 0) {
+                memset(buf, 0, sizeof(buf));
+                gets(buf, sizeof(buf));
+                if(buf[0] == 0) // EOF
+                    buffread = -1;
+                buf[strlen(buf)-1] = 0;
+                break;
+            }
+        }
+        
       exec("sh", argv);
       printf(1, "init: exec sh failed\n");
       exit();
